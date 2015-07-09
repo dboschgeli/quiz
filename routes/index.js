e421d4498ff740143e2ee007a098f19c5d0ed39e
dboschgeli <dboschgeli@gmail.com>
2015-07-09 21:28:43 +0200

    Primera edicion y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bienvenido a Quiz', subtitle: 'El portal donde podrá crear sus propias adivinanzas ;-)' });
 });
 
 module.exports = router;
