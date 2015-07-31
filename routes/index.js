06ad698dcccf2dd1e1b5612f23e628c4d9d6205d
dboschgeli <dboschgeli@gmail.com>
2015-07-31 19:36:13 +0200

    borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,7 @@ router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);//POST
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update); //PUT
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy); //Delete
 
 
 module.exports = router;
