2fea782d33f8ecd95a0b13a1560aacf086dc8756
dboschgeli <dboschgeli@gmail.com>
2015-07-31 20:53:58 +0200

    crear comentarios

--- models/models.js
+++ models/models.js
@@ -26,13 +26,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
 
+
+Comment.belongsTo(Quiz); // cada comment pertenece a un quiz
+Quiz.hasMany(Comment);   // un quiz puede tener muchos comments
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar tabla Comment
 
 
 
